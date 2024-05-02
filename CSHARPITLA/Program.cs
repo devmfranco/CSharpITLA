0552676107dceead5c3315eb47ca04041bb382f9
devmfranco <108529460+devmfranco@users.noreply.github.com>
2024-05-02 13:22:08 -0400

    Programa funcional

--- CSHARPITLA/Program.cs
+++ CSHARPITLA/Program.cs
@@ -1,3 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int primerNumero, segundoNumero, suma, producto;
+
+        Console.WriteLine("Introduzca el primer numero");
+        primerNumero=Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Introduzca el segundo numero");
+        segundoNumero=Convert.ToInt32(Console.ReadLine());
+
+
+        suma=primerNumero+segundoNumero;
+        producto=primerNumero*segundoNumero;
+
+        Console.WriteLine("El resultado de la suma es : " +suma);
+        Console.WriteLine("El resultado del producto es: " + producto); 
+
+        
+    }
+}
--- CSHARPITLA/obj/Debug/net8.0/CSHARPITLA.AssemblyInfo.cs
+++ CSHARPITLA/obj/Debug/net8.0/CSHARPITLA.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSHARPITLA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ffad0688b196c60b9b2786530ad2a6602f5751d")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSHARPITLA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSHARPITLA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
